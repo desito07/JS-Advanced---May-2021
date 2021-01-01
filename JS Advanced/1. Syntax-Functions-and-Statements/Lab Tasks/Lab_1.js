f5263dce9fcec9d83a0e843fb43afd057145d786
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-01 19:52:17 +0000

    Add files via upload

---
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_1.js
@@ -0,0 +1,12 @@
+function solve(str1, str2, str3){
+    let sum = 0;
+    let avr = 0;
+    let firstLength = str1.length;
+    let secondLength = str2.length;
+    let thirdLength = str3.length;
+    sum = firstLength+secondLength+thirdLength;
+    avr = Math.floor(sum / 3);
+    console.log(sum);    
+    console.log(avr);
+}
+solve('chocolate', 'ice cream', 'cake');
