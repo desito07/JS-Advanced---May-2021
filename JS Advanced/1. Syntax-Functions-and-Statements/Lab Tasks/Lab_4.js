f7b94ea8f4be4695c02b36ac4f133bac251d821e
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-01 19:52:43 +0000

    Add files via upload

---
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_4.js
@@ -0,0 +1,5 @@
+function findMaxNumber(first, second, third){
+    let maxNumber = Math.max(first, second, third);
+    console.log(`The largest number is ${maxNumber}.`);
+}
+findMaxNumber(5, -3, 16);
