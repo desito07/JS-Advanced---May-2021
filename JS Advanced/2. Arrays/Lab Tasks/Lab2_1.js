80ac3cf65b6692f2676861d3cfc4250bc8e0c08c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-07 22:37:23 +0000

    Add files via upload

---
+++ JS Advanced/2. Arrays/Lab Tasks/Lab2_1.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+    let fisrtArg = Number(arr[0]);
+    let lastArg = Number(arr[arr.length - 1]);
+    let sum = fisrtArg + lastArg;
+    console.log(sum);
+  }
+  solve(["20", "30", "40"]);
+  
