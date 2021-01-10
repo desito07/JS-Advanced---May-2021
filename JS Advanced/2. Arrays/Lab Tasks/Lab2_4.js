6121ab80ce715de5316bca195862eb3d03bb21e8
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-10 17:36:57 +0000

    Add files via upload

---
+++ JS Advanced/2. Arrays/Lab Tasks/Lab2_4.js
@@ -0,0 +1,12 @@
+function solve(n, k) {
+  let result = [1];
+
+  for (let i = 1; i < n; i++) {
+    let lastKelements = result.slice(-k);
+    let sum = lastKelements.reduce((a, x) => a + x, 0);
+
+    result.push(sum);
+  }
+  console.log(result.join(" "));
+}
+solve(6, 3);
