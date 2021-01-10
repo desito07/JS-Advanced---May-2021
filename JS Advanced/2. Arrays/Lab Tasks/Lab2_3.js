1963a16dca4c5816f5d1641f93d3c371d3bd0612
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-10 13:04:30 +0000

    Update Lab2_3.js

--- JS Advanced/2. Arrays/Lab Tasks/Lab2_3.js
+++ JS Advanced/2. Arrays/Lab Tasks/Lab2_3.js
@@ -1,4 +1,12 @@
-function solve(arr){
-    
-}
-solve(['7', '-2', '8', '9']);
+function solve(arr) {
+    let newArr = [];
+    arr.forEach((el) => {
+      if (el < 0) {
+        newArr.unshift(el);
+      } else {
+        newArr.push(el);
+      }    
+    });
+    console.log(newArr.join('\n'));
+  }
+  solve([7, -2, 8, 9]);
