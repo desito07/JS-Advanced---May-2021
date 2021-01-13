6551c0ff734bf01814e24966af269ef7047d0d90
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-13 23:36:14 +0000

    Update Lab_5.js

--- JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_5.js
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_5.js
@@ -10,3 +10,29 @@ function solve(arg) {
 }
 solve(5);
 solve("name");
+
+function solve(num1, num2, operator) {  
+    let result = 0;
+    switch(operator){
+      case '+':
+      result = num1 + num2;
+      break;
+      case '-':
+      result = num1 - num2;
+      break;
+      case '*':
+      result = num1 * num2;
+      break;
+      case '/':
+      result = num1 / num2;
+      break;
+      case '%':
+      result = num1 % num2;
+      break;
+      case '**':
+      result = num1 ** num2;
+      break;
+    }
+    console.log(result);    
+}
+solve(5, 6, '+');
