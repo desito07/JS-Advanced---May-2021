932ed3574f5ba199e9793a2f86af4bd75218b295
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-13 17:05:25 +0000

    Update Lab_2.js

--- JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_2.js
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_2.js
@@ -18,3 +18,14 @@ function solve(firstNum, secondNum, operator){
 }
 solve(5, 6, '+');
 solve(3, 5.5, '*');
+
+function solve(str1, str2, str3) {
+    let str1Length = Number(str1.length);
+    let str2Length = Number(str2.length);
+    let str3Length = Number(str3.length);
+    let sum = Math.floor(str1Length+str2Length+str3Length);
+    let avr = Math.floor(sum / 3);
+    console.log(sum);
+    console.log(avr);    
+}
+solve('pasta', '5', '22.3');
