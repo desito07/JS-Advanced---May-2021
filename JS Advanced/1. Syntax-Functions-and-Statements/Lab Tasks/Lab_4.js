efa9da7d01629a0d915da0f9e9aa6fc82638109c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-13 23:30:41 +0000

    Update Lab_4.js

--- JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_4.js
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_4.js
@@ -3,3 +3,17 @@ function findMaxNumber(first, second, third){
     console.log(`The largest number is ${maxNumber}.`);
 }
 findMaxNumber(5, -3, 16);
+
+function solve(input) {
+  let arg = typeof input;
+  // console.log(arg);
+  if (arg === "number") {
+    console.log((Math.PI * input * input).toFixed(2));
+  } else {
+    console.log(
+      `We can not calculate the circle area, because we receive a ${arg}.`
+    );
+  }
+}
+solve(5);
+solve("name");
