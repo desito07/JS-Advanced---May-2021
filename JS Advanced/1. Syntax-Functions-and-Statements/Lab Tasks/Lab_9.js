5e537b8cbcfd434a323897b5a83198ba4ffe3c12
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-01-14 00:08:27 +0000

    Update Lab_9.js

--- JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_9.js
+++ JS Advanced/1. Syntax-Functions-and-Statements/Lab Tasks/Lab_9.js
@@ -9,3 +9,20 @@ function solve(text) {
 }
 solve("Hi, how are you?");
 solve("hello");
+
+function solve(input) {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    sum += input[i];
+  }
+  console.log(sum); 
+  let inverted = 0;
+  for (const element of input) {
+     inverted += 1 / element;
+  }
+  console.log(inverted);  
+  console.log(input.join("")); 
+}
+solve([1, 2, 3]);
+
+
