82bf06a141223a2844b8a188a8f05e76a058c19c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-02-01 22:48:52 +0000

    Add files via upload

---
+++ JS Advanced/3. Objects-and-Classes/Exercise Tasks/Exercise3_3.js
@@ -0,0 +1,35 @@
+function solve(input) {
+    let result = {};
+    let juicesInOrder = {};
+
+    input.forEach(e => {
+        let [product, quantity] = e.split(' => ');
+
+        if(!result[product]){
+            result[product] = Number(quantity);
+        } else {
+            result[product] += Number(quantity);
+        }
+
+        if(result[product] >= 1000){
+            juicesInOrder[product] = 0;
+        }
+    });
+
+    Object.keys(result).forEach(j => {
+        if(juicesInOrder[j] !== undefined){
+            juicesInOrder[j] += parseInt(result[j] / 1000);
+        }
+});
+
+    Object.keys(juicesInOrder).forEach(j => {
+        console.log(`${j} => ${juicesInOrder[j]}`);
+    });
+    
+}
+solve(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549']
+    );
