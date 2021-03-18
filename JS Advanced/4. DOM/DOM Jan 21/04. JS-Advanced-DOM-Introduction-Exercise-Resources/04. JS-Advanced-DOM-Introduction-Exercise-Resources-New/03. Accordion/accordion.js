a44c748cd692fffdfaa826195f182f13c0b2821b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-03-18 22:13:05 +0000

    Update accordion.js

--- JS Advanced/4. DOM/DOM Jan 21/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ JS Advanced/4. DOM/DOM Jan 21/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,19 @@
 function toggle() {
-    console.log('TODO:...');
+  const button = document.getElementsByClassName("button")[0];
+  //   console.log(button.textContent);
+
+  const element = document.getElementById("extra");
+  //   console.log(element);
+
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    element.style.display = "none";
+  }
+
+  if (button.textContent === "More") {
+    button.textContent = "Less";
+  } else {
+    button.textContent = "More";
+  }
 }
