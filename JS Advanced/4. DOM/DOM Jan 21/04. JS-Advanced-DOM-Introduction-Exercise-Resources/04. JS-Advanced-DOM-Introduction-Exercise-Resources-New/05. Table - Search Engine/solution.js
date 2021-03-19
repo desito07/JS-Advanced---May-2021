7bf0fbbc615320ba763fea9db9c00fc8b17426a7
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2021-03-19 23:21:07 +0000

    Update solution.js

--- JS Advanced/4. DOM/DOM Jan 21/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ JS Advanced/4. DOM/DOM Jan 21/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,18 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  const rows = document.querySelectorAll("tbody tr");
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    const input = document.querySelector("#searchField").value.toLowerCase();
+    //  console.log(input.value);
+    //  console.log(rows);
 
-   }
+    for (let row of rows) {
+      if (row.textContent.toLowerCase().includes(input)) {
+        row.setAttribute("class", "select");
+      } else {
+        row.removeAttribute("class");
+      }
+    }
+  }
 }
